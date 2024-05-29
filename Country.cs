b114dda47e80b3bceaab931bda3df3d5a35d06c7
hello <157564249+Hello-rtr@users.noreply.github.com>
2024-05-29 10:47:29 +0500

    Add files via upload

---
+++ Country.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prakt_16_4
+{
+    public class Country
+    {
+        private string Name { get; set; }
+        private long Population { get; set; }
+
+        public string NameValue
+        {
+            get { return Name; }
+            set { Name = value; }
+        }
+        public long PopulationValue
+        {
+            get { return Population; }
+            set { Population = value; }
+        }
+    }
+     
+}
---
+++ Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace prakt_16_4
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        bool click = false;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!click)
+            {
+                List<Country> countries = new List<Country>();
+                string[] lines = File.ReadAllLines("count.txt");
+                foreach (string line in lines)
+                {
+                    string[] parts = line.Split(' ');
+                    string name = parts[0];
+                    string populationString = string.Join("", parts.Skip(1));
+                    long population = long.Parse(populationString.Replace(" ", ""));
+
+                    countries.Add(new Country { NameValue = name, PopulationValue = population });
+                }
+                if (textBox1.Text == "")
+                {
+                    MessageBox.Show("Введите минимальную численость населения: ");
+                }
+                else
+                {
+                    if (int.TryParse(textBox1.Text, out int minPopulation))
+                    {
+                        if (minPopulation >= 1)
+                        {
+                            var sortedCountries = countries
+                               .Where(c => c.PopulationValue > minPopulation)
+                               .OrderBy(c => c.NameValue)
+                               .OrderBy(c => c.NameValue.Length);
+                            foreach (var country in sortedCountries)
+                            {
+                                listBox1.Items.Add(Convert.ToString(country.NameValue + " " + country.PopulationValue));
+                            }
+                            click = true;
+                        }
+                        else
+                        {
+                            MessageBox.Show("Введите число больше 0");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Вы ввели не число");
+                    }
+                }
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
